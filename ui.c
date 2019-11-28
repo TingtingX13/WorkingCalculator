e30802c723804db01271c5c8294bf703cf40fc29
TingtingX13 <55857311+TingtingX13@users.noreply.github.com>
2019-11-28 12:12:59 +0000

    Add files via upload

---
+++ ui.c
@@ -0,0 +1,27 @@
+/*
+    ui.c
+    Kevin Smith
+    26 November 2019
+*/
+
+#include <stdio.h>
+#include <stdlib.h> 
+#include "infixtopostfix.h"
+#include "codegen.h"
+#include "interpreter.h"
+
+int main()
+{
+	FILE *output = fopen("infix.txt", "w");
+	char buf[500]; 
+    fgets(buf, 500, stdin); 
+    printf("input: %s\n", buf); 
+    fputs(buf, output);
+    fclose(output);
+	
+	testinfixtopostfix();
+
+	codegen();
+	
+	interpret();
+}
